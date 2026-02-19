6bf7316585a70c04f9276cfe47b323b0480dc024
unknown <jubairhosain173@gmail.com>
2026-02-19 16:10:12 +0600

    js practic

--- task.js
+++ task.js
@@ -103,10 +103,142 @@
 //     console.log(date);
 // }
 
+// let num = 0;
+// while(num <= 10){
+//     console.log('i programming for the 3 hours every day', num);
+//     num++;
+// }
+
+// let nam = 1;
+// while(nam <= 20){
+//     console.log(nam);
+//     nam = nam + 1
+// }
+
+
+// let font = 50;
+// while(font <= 100){
+//     console.log(font);
+//     font++;
+// }
+
+// let sum = 1;
+// while(sum <= 10){
+//     console.log(sum * 3);
+//     sum++;
+// }
+
+
+// let f = 111;
+// while(f >= 110){
+//     console.log(f / 2);
+//     f--;
+// }
+
+
+// let number = 50;
+// while(number < 100){
+//     console.log(number);
+//     number++;
+// }
+
+// let numb = 5;
+// let sum = 0;
+// while(numb <= 15){
+//     console.log(numb);
+//     sum = sum + numb;
+//     numb++;
+//     console.log('sum', sum)
+// }
+
+// let roll = 1;
+// let serial = 0;
+// while(roll <= 50){
+//     console.log(roll);
+//     serial = serial + roll;
+//     roll++;
+//     console.log('serial number', serial);
+// }
+
+
+// let w = 21;
+// let r = 0;
+// while(w <= 50){
+//     w++;
+//     console.log(w);
+//     r = r + w;
+// }
+// console.log('total', r);
+
+// let final = 20;
+// let join = 0;
+// while(final <= 40){
+//     console.log(final);
+//     final++;
+//     join = join + final;
+//     console.log('final output', join);
+
+// }
+
+
+// for(let jam = 150; jam <= 170; jam++){
+//     console.log(jam);
+// }
+
+// let list = 0;
+// for(let student = 31; student <= 58; student++){
+//     console.log(student);
+//     list = list + student;
+// }
+// console.log(list);
+
+
+// let sum = 0;
+// for(let end = 25; end <= 75; end++){
+//     console.log(end);
+//     sum = sum + end;
+//     console.log('sum of total number', sum);
+// } 
+// console.log('Total', sum);
+
+// for(let i = 20; i <= 50; i++){
+//     if(i % 7 == 0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i = 40; i <= 80; i++){
+//     if(i % 5 == 0 || i % 7 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+// for(let fo = 1; fo <= 50; fo = fo + 4){
+//     console.log(fo)
+// }
+// for(let you = 0; you <= 100; you++){
+//     if(you % 9 == 0 && you % 6 == 0){
+//         console.log(you);
+//     }
+// }
+
+
+// let sum = 0;
+// for(let am = 1; am <= 50; am++){
+//     if(am % 3 == 0 && am % 4 == 0){
+//         console.log(am)
+//         sum = sum + am;
+//     }
+// }
+// console.log("Total", sum);
 
 
 
-
-
-
+for(let odd = 1; odd <= 30; odd++){
+    console.log(odd);
+    if(odd >= 15){
+        break;
+    }
+}
 
